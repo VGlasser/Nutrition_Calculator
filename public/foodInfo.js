a48517bafd79e71e1e652d73a1e4bb8e234a71d9
Victor <victorglasser@hotmail.com>
2024-08-15 18:28:48 -0400

    Portion Selection Functioning
    Portion selection works well enough. Buttons don't work yet and selection clutters screen, but works well enough for now.

--- public/foodInfo.js
+++ public/foodInfo.js
@@ -1,32 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
     const urlParameters = new URLSearchParams(window.location.search);
     var id = urlParameters.get('id')||0;
-    console.log(foodList[id].FoodName);
-
-
 
     function displayPage() {
 
         document.getElementById("Name").textContent = foodList[id].FoodName;
 
+        portions = foodList[id].portions;
 
-        console.log("success")
-        // const pageItems = Object.entries(foodList);
-        // const tableBody = document.querySelector('#data-table tbody');
-        // tableBody.innerHTML = ''; 
+        
+        const tableBody = document.getElementById('PortionTable'); 
 
-        // pageItems.forEach(food => {
-        //     const foodDetails = food[1];
-        //     const foodID = food[0];
-        //     const row = document.createElement('tr');
-        //     row.innerHTML = `
-        //         <td><a href="/account/${userAccount.Username}/foodInfo/?id=${foodID}">${foodDetails.FoodName}</a></td>
-        //     `;
-        //     tableBody.appendChild(row);
-        // });
+        for (const portion in portions) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>
+                    <input type="radio" name="portion" value="${portion}"> ${portion}
+                </td>
+            `;
+            tableBody.appendChild(row);
+        }
+        const buttonRow = document.createElement('tr');
+        buttonRow.innerHTML = `
+        <td>
+            <button onclick="addToDailyTotal()">Add to my daily total</button>
+        </td>`;
+        tableBody.appendChild(buttonRow);
     }
 
     displayPage();
 });
+
+function addToDailyTotal(){
+    const selectedPortion = document.querySelector('input[name="portion"]:checked');
+    const successMessages = document.getElementById('SuccessMessages');
+    
+    if (selectedPortion) {
+        const portionValue = selectedPortion.value;
+        const successMessageRow = document.createElement('tr');
+        successMessageRow.innerHTML = `
+        <td>
+            successfully added ${portionValue} to daily total
+        </td>`;
+        successMessages.appendChild(successMessageRow);
+    } else {
+        alert('No portion selected');
+    }
+}
 
 
